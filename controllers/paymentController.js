a5520417342eac37369f891c9e8c4d1c209e4502
unknown <dev.ztyle@gmail.com>
2024-09-29 20:51:11 +0530

    payment completed

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const Razorpay = require('razorpay')
+const crypto = require('crypto')
+
 
 //models
 const paymentModel = require('../models/paymentModel')
@@ -45,6 +48,11 @@ const getServicePrice = asyncHandler(async (req, res) => {
         throw new Error('Service Not Found')
     }
 
+    if (currentBooking.status !== "confirmed") {
+        res.status(400)
+        throw new Error('Cannot Make Payment Since Your Booking Is Not Confirmed')
+    }
+
     const paymentDetails = {
         amount : currentService.price + 5,
         splitAmount : currentService.price,
@@ -62,8 +70,166 @@ const getServicePrice = asyncHandler(async (req, res) => {
     
 })
 
+const addOrder = asyncHandler(async (req, res) => {
+
+    const { amount, splitAmount, accountId } = req.body
+    
+    // validate the fields
+    if (!amount || !splitAmount || !splitAmount || !accountId) {
+        res.status(404)
+        throw new Error('At Least One Field Is Empty')
+
+    }
+    
+    // create razorpay instance
+    const instance = new Razorpay({
+        key_id: process.env.KEY_ID,
+        key_secret:process.env.KEY_SECRET
+    })
+
+    const options = {
+        amount: amount * 100,
+        currency: 'INR',
+        receipt: crypto.randomBytes(10).toString('hex'),
+        transfers: [
+            {
+            
+                account: accountId,
+                amount: splitAmount * 100,
+                currency: 'INR',
+                notes: {
+                    branch: 'Ole',
+                    name:'Ole Group'
+                },
+                on_hold:0
+            }
+        ]
+    }
+
+    instance.orders.create(options, (error, order) => {
+        if (error) {
+            console.log(error)
+            return res.status(500).json({message:"Something Went Wrong"})
+        }
+        res.status(200).json(order)
+    })
+
+    
+})
+
+const verifyOrder = asyncHandler(async (req, res) => {
+    
+    const {
+        razorpay_order_id, razorpay_payment_id,
+        razorpay_signature, bookingId, userId, amount,
+        currency,paymentMethod
+    } = req.body
+
+    // validation
+    if (!razorpay_order_id || !razorpay_payment_id || !razorpay_signature || !bookingId || !userId || !amount || !currency || !paymentMethod) {
+        res.status(404)
+        throw new Error('At Least One Field Is Empty')
+    }
+
+    // verify the razorpay signature
+    const sign = razorpay_order_id + '|' + razorpay_payment_id
+    const expectedSign = crypto.createHmac("sha256", process.env.KEY_SECRET).update(sign.toString()).digest('hex')
+
+    if (razorpay_signature === expectedSign) {
+        // payment data object
+
+        const newPayment = {
+            paymentId:razorpay_payment_id,
+            orderId: razorpay_order_id,
+            bookingId,
+            userId,
+            amount,
+            currency,
+            paymentMethod,
+            status:"success"
+        }
+
+        await paymentModel.create(newPayment)
+        await bookingModel.findByIdAndUpdate(bookingId, { status: 'completed' })
+        
+        const payment = await paymentModel.findOne({ bookingId })
+        if (payment) {
+            res.status(200).json(payment)
+        }
+        else {
+            res.status(400)
+            throw new Error("Invalid Payment")
+        }
+        
+    }
+    else {
+        res.status(500)
+        throw new Error("Invalid Signature .")
+    }
+    
+    
+})
+
+const getPaymentById = asyncHandler(async (req, res) => {
+
+    const { id } = req.params
+    
+    // access the booking with specified id
+    const payment = await paymentModel.findById(id)
+    if (payment) {
+        res.status(200).json(payment)
+    }
+    else {
+        res.status(404)
+        throw new Error('Payment Not Found')
+    }
+})
+
+const getPaymentByBookingId = asyncHandler(async (req, res) => {
+
+    const { id } = req.params
+    
+    // access the booking with specified id
+    const payment = await paymentModel.findOne({ bookingId: id })
+    
+    if (payment) {
+        res.status(200).json(payment)
+    }
+    else {
+        res.status(404)
+        throw new Error('Payment Not Found')
+    }
+})
+
+const getPaymentByByField = asyncHandler(async (req, res) => {
+    
+    //access field and value 
+    const { field, value } = req.body
+    // write cquery to select the field
+    const query = {
+        [field]:value
+    }
+
+    const payments = await paymentModel.find(query)
+
+    if (payments.length !== 0) {
+        res.status(200).json(payments)
+    }
+    else {
+        res.status(400)
+        throw new Error('No Payments Found For Specified Field')
+    }
+
+})
+
 
 module.exports = {
     PaymentHome,
-    getServicePrice
+    getServicePrice,
+    addOrder,
+    verifyOrder,
+    getPaymentById,
+    getPaymentByBookingId,
+    getPaymentByByField
+    
 }
--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -2,15 +2,38 @@ const express = require('express')
 const router = express.Router()
 
 // auth middleware jwt
-const {protectUser} =require('../middleware/authMiddleware')
+const {
+    protectUser,
+    protectBeautician,
+    protectAdmin } = require('../middleware/authMiddleware')
 
 // controllers
 const {
     PaymentHome,
-    getServicePrice } = require('../controllers/paymentController')
+    getServicePrice,
+    addOrder,
+    verifyOrder,
+    getPaymentById,
+    getPaymentByBookingId,
+    getPaymentByByField } = require('../controllers/paymentController')
 
 router.route('/').all(protectUser, PaymentHome)
+// user access
 router.route('/service-price/:id').get(protectUser, getServicePrice)
+router.route('/orders').post( addOrder)
+router.route('/verify').post(verifyOrder)
+router.route('/user-payments/:id').get(protectUser, getPaymentById)
+router.route('/user-payments/by-booking/:id').get(protectUser, getPaymentByBookingId)
+router.route('/user-payments/by-field').post(protectUser, getPaymentByByField)
+// beautician access
+router.route('/beautician-payments/:id').get(protectBeautician, getPaymentById)
+router.route('/beautician-payments/by-booking/:id').get(protectBeautician, getPaymentByBookingId)
+router.route('/beautician-payments/by-field').post(protectBeautician, getPaymentByByField)
+// admin access
+router.route('/admin-payments/:id').get(protectAdmin, getPaymentById)
+router.route('/admin-payments/by-booking/:id').get(protectAdmin, getPaymentByBookingId)
+router.route('/admin-payments/by-field').post(protectAdmin, getPaymentByByField)
+
 
 
 module.exports = router
